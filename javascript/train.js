034f50028cedc6c3888871561b76cc6dee1dd737
reb2039607 <reb.aguayo@gmail.com>
2019-10-01 18:37:36 -0700

    created general page layout, set up firebase app

---
+++ javascript/train.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCadCUMSmvitqcibC-nbBk2pO4r3G7hE-A",
+    authDomain: "train-scheduler-15d2b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-15d2b.firebaseio.com",
+    projectId: "train-scheduler-15d2b",
+    storageBucket: "",
+      };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const database = firebase.database();
+
+  
+  
